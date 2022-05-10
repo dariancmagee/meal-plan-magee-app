7adeb8e4850065690a831b92c5269f9e4a10b303
dariancmagee <dariancmagee@gmail.com>
2022-05-09 20:42:39 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import MealList from "./MealList";
+
+const mealKey = '4d3f246f2f71445ea8ca44032ed321c2'
 
 function App() {
+  const [mealData, setMealData] = useState(null);
+  const [calories, setCalories] = useState(2000);
+
+  function handleChange(e) {
+    setCalories(e.target.value);
+  }
+
+  function getMealData() {
+    fetch(
+      `https://api.spoonacular.com/mealplanner/generate?apiKey=${mealKey}&timeFrame=day&targetCalories=${calories}`
+    )
+    .then((response) => response.json())
+    .then((data) => {
+      setMealData(data);
+      console.log(data);
+    })
+    .catch(() => {
+      console.log("error");
+    });
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Meal Plan Magee</h1>
+      <section className="controls">
+        <input 
+        type="number"
+        placeholder="Calories (e.g. 2000)"
+        onChange={handleChange} />
+      </section>
+      <button onClick={getMealData}>Get Daily Meal Plan</button>
+      {mealData && <MealList mealData={mealData} />}
     </div>
   );
 }
---
+++ src/Meal.js
@@ -0,0 +1,30 @@
+import React, {useState, useEffect} from 'react';
+
+export default function Meal({meal}) {
+    const [imageUrl, setImageUrl] = useState("");
+
+    useEffect(() => {
+        fetch(
+            `https://api.spoonacular.com/recipes/${meal.id}/information?apiKey=4d3f246f2f71445ea8ca44032ed321c2&includeNutrition=false`
+        )
+        .then((response) => response.json())
+        .then((data) => {
+            setImageUrl(data.image);
+        })
+        .catch(() => {
+            console.log("error");
+        })
+    }, [meal.id])
+    return (
+    <article>
+        <h2>{meal.title}</h2>
+        <img src={imageUrl} alt="recipe" />
+        <ul className="instructions">
+            <li>Preparation time: {meal.readyInMinutes} minutes</li>
+            <li>Number of servings: {meal.servings}</li>
+        </ul>
+
+        <a href={meal.sourceUrl}>Go to Recipe</a>
+    </article>
+    );
+}
